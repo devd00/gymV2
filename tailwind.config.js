0ada9d6e233d6631288ea9b0572f34e510dea0a9
devd00 <joydipkn@gmail.com>
2023-03-29 10:19:49 +0530

    added the html barebones

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "backgroundGym" : "url('public\img\anastase-maragos-7kEpUPB8vNk-unsplash.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
