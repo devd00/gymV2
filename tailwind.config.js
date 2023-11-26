ab11f1aed84c54bea80b645f06288b890cba23c1
devd00 <joydipkn@gmail.com>
2023-11-26 09:12:12 +0530

    added styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage:{
-        "backgroundGym" : "url('public\img\anastase-maragos-7kEpUPB8vNk-unsplash.jpg')"
+        "backgroundGym" : "url('public\img\anastase-maragos-7kEpUPB8vNk-unsplash.jpg')",
+        "backgroundPic1" : "url('public\img\alexander-jawfox-Kl2t5U6Gkm0-unsplash.jpg')",
       }
     },
   },
